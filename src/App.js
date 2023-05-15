a1ea09e8fc4f0e0eb2a4338cdd26f2ad22d1003e
Neville Brem <contact@nevillebrem.com>
2023-05-15 19:06:23 +0200

    removed single files for product. using parameters instead

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Products from "./components/Products";
 import Info from "./components/Info";
-import ProductTemplate from "./components/products/ProductTemplate"
-import Ab from "./components/products/A_b";
-import Ag from "./components/products/A_g";
-import Atrt from "./components/products/At_rt";
-import Barc from "./components/products/Barc";
-import Crab from "./components/products/Crab";
-import Ss from "./components/products/SwampSpeeder";
-import Rancy from "./components/products/Rancy";
-import Droideka from "./components/products/Droideka"; 
+import ProductPage from "./components/products/Product"
 
 function App() {
   return (
@@ -24,15 +16,86 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/info" element={<Info />} />
-        <Route path="/products/assassin-black" element={<Ab />} />
-        <Route path="/products/assassin-gray" element={<Ag />} />
-        <Route path="/products/at-rt" element={<Atrt />} />
-        <Route path="/products/barc-speeder" element={<Barc />} />
-        <Route path="/products/droideka" element={<Droideka />} />
-        <Route path="/products/crab-droid" element={<Crab />} />
-        <Route path="/products/swamp-speeder" element={<Ss />} />
-        <Route path="/products/rancor" element={<Rancy />} /> 
-        <Route path="/admin/template" element={<ProductTemplate />} /> 
+        <Route path="/products/assassin-black" element={<ProductPage 
+          title="Black Assassin Droid"  
+          addToCart="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JE4FAGYHTQQVA" 
+          price="2" 
+          designer="William"
+          description="The Assassin Droid is ideal for hunting bounty or serving in a gang. It offers many details and is minifigure-scaled." 
+          parts="25" 
+          blPrice="~2.50" 
+          />} 
+          />
+        <Route path="/products/assassin-gray" element={<ProductPage
+         title="Gray Assassin Droid"  
+         addToCart="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=HC9LK2N8FJ5AY" 
+         price="2" 
+         designer="William"
+         description="The Assassin Droid is ideal for hunting bounty or serving in a gang. It offers many details and is minifigure-scaled." 
+         parts="25" 
+         blPrice="~2.50" 
+         />}
+          />
+        <Route path="/products/at-rt" element={<ProductPage
+        title="AT-RT Walker"  
+        addToCart="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8BEMJHHRBVJ7G" 
+        price="5" 
+        designer="William"
+        description="This All-Terrain Recon Transporter is ideal for every MOC. Since it is not too big, you can build it multiple times and display or put it in your base or battle field! It is capable of holding a trooper and 2 weapons!" 
+        parts="71" 
+        blPrice="~6.50"
+        />}
+          />
+        <Route path="/products/barc-speeder" element={<ProductPage
+         title="BARC Speeder"  
+         addToCart="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=J4PKAF4NKQB9E" 
+         price="7.50" 
+         designer="William"
+         description="The BARC (Biker Advanced Recon Commando) Speeder are light and optimal to use them in quantity for your Mocs!" 
+         parts="119" 
+         blPrice="~18" 
+         />}
+          />
+        <Route path="/products/crab-droid" element={<ProductPage
+         title="Crab Droid"  
+         addToCart="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=GTA2WX6LF6SGQ" 
+         price="3.50" 
+         designer="William"
+         description="The Crab Droid is an important part of the separatist droid army and was designed by the Techno-Union. It offers many details and is minifigure-scaled." 
+         parts="78" 
+         blPrice="~8" 
+         />}
+          />
+        <Route path="/products/droideka" element={<ProductPage
+         title="Droideka"  
+         addToCart="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4WJH9VBUFW36E" 
+         price="3" 
+         designer="William"
+         description="The high-detailed and minifig-scaled Droideka served in the droid army of the Seperatists in the clone wars. You can roll him up, like in the movies." 
+         parts="35" 
+         blPrice="~4" 
+         />}
+          />
+        <Route path="/products/swamp-speeder" element={<ProductPage
+         title="Republic Swamp Speeder"  
+         addToCart="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7K7CSGSYGRZB6" 
+         price="10" 
+         designer="Neville"
+         description="The Republic Swamp Speeder or Infantry Support Platform (short ISP) grants protection for your troops on Felucia or other planets. It is highly-detailed." 
+         parts="104" 
+         blPrice="~40" 
+         />}
+          />
+        <Route path="/products/rancor" element={<ProductPage
+         title="Rancor"  
+         addToCart="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BGAMEEEH4KJ2U" 
+         price="20" 
+         designer="William"
+         description="The Assassin Droid is ideal for hunting bounty or serving in a gang. It offers many details and is minifigure-scaled." 
+         parts="392" 
+         blPrice="~45 CHF" 
+         />}
+          /> 
       </Routes>
       <Footer />
     </div>
--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
-import img_main from '../../assets/ab_front.png'
-import img_2 from '../../assets/ab_back.png'
-import img_3 from '../../assets/original/ig-88.png'
 
-const Template = () => {
-
-  const title = `Test`
-  const addToCart = `Test`
-  const price = `Test`
-  const description = `Test`
-  const parts = ``
-  const weapons = ``
-  const bl_price = ``
+const Template = ({ title, addToCart, price, description, parts, blPrice, img_main, img_2, img_3, designer }) => {
 
 
   return (
@@ -21,15 +10,14 @@ const Template = () => {
         <div className="my-auto">
           <h1 className="productTitle">{title} - Instructions only</h1>
           <div className="productDescContainer">
-          <a href={addToCart} className="productATCOutline"><p className="productATC">{price} - Add to cart</p></a>
+          <a href={addToCart} className="productATCOutline"><p className="productATC">{price} CHF - Add to cart</p></a>
           </div>
           <h2 className="productH2">Short description:</h2>
-          <p className="text-sm md:text-lg mb-4">{description}</p>
+          <p className="text-sm md:text-lg mb-4">Designed by {designer} <br /> {description}</p>
           <h2 className="productH2">Details:</h2>
           <ul className="productList">
               <li>{parts} Parts</li>
-              <li>{weapons} Weapons</li>
-              <li>Bricklink price: {bl_price} CHF</li>
+              <li>Bricklink price: {blPrice} CHF</li>
           </ul>
         </div>
     </div>
