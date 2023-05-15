bc2de594ead9a9e5a94b64a58f8bf7895a24a337
Neville Brem <contact@nevillebrem.com>
2023-05-15 19:33:44 +0200

    small price tag fix

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -14,14 +14,14 @@ const Products = () => {
     <div>
         <h1 className='text-4xl md:text-6xl font-black uppercase my-16'>Products</h1>
     <div id="products" className="flex justify-center gap-4 flex-wrap my-4 2xl:max-w-[80%] mx-auto">
-        <Panel link={"/products/at-rt"}         image={Atrt}            title={"AT-RT Walker"}              price={"5 CHF"} />
+        <Panel link={"/products/at-rt"}         image={Atrt}            title={"AT-RT Walker"}              price={"5.-- CHF"} />
         <Panel link={"/products/barc-speeder"}  image={Barcspeeder}     title={"BARC Speeder"}              price={"7.50 CHF"} />
-        <Panel link={"/products/crab-droid"}    image={Crabdroid}       title={"Crab Droid"}                price={"5 CHF"} />
-        <Panel link={"/products/rancor"}        image={Rancor}          title={"Rancor - Minifig Scale"}    price={"5 CHF"} />
-        <Panel link={"/products/droideka"}      image={Droideka}        title={"Droideka"}                  price={"5 CHF"} />
-        <Panel link={"/products/assassin-gray"} image={Ag}              title={"Gray Assassin Droid"}       price={"5 CHF"} />
-        <Panel link={"/products/assassin-black"}image={Ab}              title={"Black Asssassin Droid"}     price={"5 CHF"} />
-        <Panel link={"/products/swamp-speeder"} image={Swampspeeder}    title={"Swampspeeder"}              price={"5 CHF"} />
+        <Panel link={"/products/crab-droid"}    image={Crabdroid}       title={"Crab Droid"}                price={"3.50 CHF"} />
+        <Panel link={"/products/rancor"}        image={Rancor}          title={"Rancor - Minifig Scale"}    price={"20.-- CHF"} />
+        <Panel link={"/products/droideka"}      image={Droideka}        title={"Droideka"}                  price={"3.-- CHF"} />
+        <Panel link={"/products/assassin-gray"} image={Ag}              title={"Gray Assassin Droid"}       price={"2.-- CHF"} />
+        <Panel link={"/products/assassin-black"}image={Ab}              title={"Black Asssassin Droid"}     price={"2.-- CHF"} />
+        <Panel link={"/products/swamp-speeder"} image={Swampspeeder}    title={"Swampspeeder"}              price={"10.-- CHF"} />
     </div>
     </div>
   )
